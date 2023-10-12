f07227e365d144032285f1c4b0cfe6f4d2c02cb2
Sharmin Akter <129578040+S-Popy@users.noreply.github.com>
2023-10-12 19:29:38 +0600

    Add files via upload

--- week-2/the_clock.cpp
+++ week-2/the_clock.cpp
@@ -10,6 +10,27 @@ signed main() {
     int t;
     cin >> t;
     while(t--){
+        int a, b, x, y, t, ans = 0;
+        char ch;
+        cin>> a >> ch >> b >> t;
+        x = a;
+        y = b;
+
+        do{
+            if(x%10 == y/10 && x /10 == y % 10)
+            ans ++;
+            y += t%60;
+            
+            if(y>=60){
+                y -= 60;
+                x++;
+            }
+                x+= t/60;
+                x%= 24;
+            
+        }while (x !=a || y != b);
+        cout<< ans <<endl;
+        
     }
     return 0;
 }
